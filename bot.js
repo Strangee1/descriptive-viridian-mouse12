e70295908d5a34412eff27049239273d1e118028
DON15NINO <44461351+DON15NINO@users.noreply.github.com>
2020-06-20 02:48:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,45 +95,7 @@ __**Copyright FanBot**__
  }
  });
 
-////
-
-const { Client, RichEmbed } = require("discord.js");
-var { Util } = require('discord.js');
-const {prefix} = require('./config')
-const client = new Client({ disableEveryone: true})
-const ytdl = require("ytdl-core");
-const canvas = require("canvas");
-const Canvas = require("canvas");
-const convert = require("hh-mm-ss")
-const fetchVideoInfo = require("youtube-info");
-const botversion = require('./package.json').version;
-const simpleytapi = require('simple-youtube-api')
-const moment = require("moment");
-const fs = require('fs');
-const util = require("util")
-const gif = require("gif-search");
-const opus = require("node-opus");
-const ms = require("ms");
-const jimp = require("jimp");
-const { get } = require('snekfetch');
-const guild = require('guild');
-const dateFormat = require('dateformat');//npm i dateformat
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyCx7drgLYZ-GSMUToLXBo4oWBAC6cUP7AA');
-const hastebins = require('hastebin-gen');
-const getYoutubeID = require('get-youtube-id');
-const yt_api_key = "AIzaSyCx7drgLYZ-GSMUToLXBo4oWBAC6cUP7AA";
-const pretty = require("pretty-ms");
-client.login(process.env.BOT_TOKEN);
-const queue = new Map();
-var table = require('table').table
-const Discord = require('discord.js');
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-//code
+//cod
 
 
 client.commands = new Discord.Collection();
